212d9a7acb53edd044abd62c5a3978c0197f8ba3
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2020-03-18 21:14:46 +0100

    First test... gone wrong

---
+++ src/com/expertsystem/Main.java
@@ -0,0 +1,35 @@
+package com.expertsystem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        ArrayList<Rule> rules = new ArrayList<>(Arrays.asList(
+                new Rule("Bicycle",
+                        new ArrayList<Statement>(Arrays.asList(
+                                new Statement("vehicleType", Type.STRING, Operation.EQ, "cycle"),
+                                new Statement("num_wheels", Type.INTEGER, Operation.EQ, 4),
+                                new Statement("motor", Type.BOOLEAN, Operation.EQ, false)
+                        )),
+                        new ArrayList<Statement>(Arrays.asList(
+                                new Statement("vehicle", Type.STRING, Operation.EQ, "bicycle")
+                        )))
+        ));
+
+        ArrayList<Statement> knowledge = new ArrayList<>(Arrays.asList(
+                new Statement("vehicleType", Type.STRING, Operation.EQ, "cycle"),
+                new Statement("num_wheels", Type.INTEGER, Operation.EQ, 4),
+                new Statement("motor", Type.BOOLEAN, Operation.EQ, false)
+        ));
+
+        Statement goal = new Statement("vehicle", Type.STRING, Operation.EQ, "bicycle");
+
+        ExpertSystem ES = new ExpertSystem(rules, knowledge);
+
+        System.out.println(ES.infer(goal));
+
+
+    }
+}
