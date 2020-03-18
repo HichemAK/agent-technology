1219e717d2ae3272b18ba1ead7a82b63cb357758
ouss1002 <oboudjeniba@gmail.com>
2020-03-18 18:37:30 +0100

    Finish Rule & ExpertSystem classes

--- src/com/expert_system/Rule.java
+++ src/com/expert_system/Rule.java
@@ -1,4 +1,32 @@
 package com.expert_system;
 
 public class Rule {
+
+    private String name;
+    private ArrayList<Statements> antecedents;
+    private ArrayList<Statements> consequences;
+
+    public Rule(String name, ArrayList<Statements> statements, ArrayList<Statements> consequences) {
+        this.name = name;
+        this.antecedents = statements;
+        this.consequences = consequences;
+    }
+
+    public Rule(String name) {
+        this.name = name;
+        antecedents = new ArrayList<Statement>();
+        consequences = new ArrayList<Statement>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Statements> getAntecedents() {
+        return antecedents;
+    }
+
+    public ArrayList<Statements> getConsequences() {
+        return consequences;
+    }
 }
