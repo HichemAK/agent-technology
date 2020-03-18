b6441ba18723fcc005463a75442726dd915fec92
ouss1002 <oboudjeniba@gmail.com>
2020-03-18 22:17:27 +0100

    change app name

--- src/com/app/App.java
+++ src/com/app/App.java
@@ -3,7 +3,7 @@ package com.app;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class application extends Application {
+public class App extends javafx.application.Application {
 
     public static void main(String[] args) {
         launch(args);
