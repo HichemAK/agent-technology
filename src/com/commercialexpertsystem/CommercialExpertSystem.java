94540bfafef5b540b5a54089cd87a27a0f4a8f7c
ouss1002 <oboudjeniba@gmail.com>
2020-03-24 13:56:28 +0100

    Add CommercialExpertSystem Class

---
+++ src/com/commercialexpertsystem/CommercialExpertSystem.java
@@ -0,0 +1,11 @@
+package com.commercialexpertsystem;
+
+import com.expertsystem.ExpertSystem;
+
+public class CommercialExpertSystem extends ExpertSystem {
+
+    public CommercialExpertSystem() {
+
+    }
+
+}
