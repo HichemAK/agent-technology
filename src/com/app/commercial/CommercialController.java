21b35e14698181d08c5adbda5e31f088156556f0
AMMAR KHODJA Hichem <hichem5696@gmail.com>
2020-03-24 20:24:50 +0100

    stock

--- src/com/app/commercial/CommercialController.java
+++ src/com/app/commercial/CommercialController.java
@@ -34,12 +34,12 @@ public class CommercialController {
     public TextField tfBudget;
 
     private ExpertSystem ES = new ExpertSystem("PC.es");
-    private HashMap<String, Boolean> availability = new HashMap<>();
+    private HashMap<String, Integer> stock = new HashMap<>();
     private ArrayList<String> amd_cpus = new ArrayList<>(Arrays.asList("None", "Ryzen3", "Ryzen5", "Ryzen7"));
     private ArrayList<String> intel_cpus = new ArrayList<>(Arrays.asList("None", "Pentium", "DualCore", "i3", "i5", "i7"));
     private ArrayList<String> gpus = new ArrayList<>(Arrays.asList("None", "RTX", "GTX", "Radeon", "Titan"));
     private ArrayList<String> rams = new ArrayList<>(Arrays.asList("0", "1", "2", "4", "8", "12", "16", "32", "64", "128"));
-    private int ram = 96;
+    private HashMap<String, Integer> price = new HashMap<>();
 
     public CommercialController() throws Exception {
     }
@@ -49,25 +49,26 @@ public class CommercialController {
     }
 
     public void initialize(){
-        availability.put("Ryzen3", true);
-        availability.put("Ryzen5", false);
-        availability.put("Ryzen7", true);
-        availability.put("Pentium", false);
-        availability.put("DualCore", true);
-        availability.put("i3", true);
-        availability.put("i5", true);
-        availability.put("RTX", false);
-        availability.put("GTX", true);
-        availability.put("Radeon", true);
-        availability.put("Titan", false);
-        availability.put("mouse", true);
-        availability.put("keyboard", true);
-        availability.put("hdd256", false);
-        availability.put("hdd512", false);
-        availability.put("hdd1024", true);
-        availability.put("ssd256", true);
-        availability.put("ssd512", true);
-        availability.put("ssd1024", false);
+        stock.put("Ryzen3", 10);
+        stock.put("Ryzen5", 0);
+        stock.put("Ryzen7", 8);
+        stock.put("Pentium", 0);
+        stock.put("DualCore", 4);
+        stock.put("i3", 20);
+        stock.put("i5", 20);
+        stock.put("RTX", 0);
+        stock.put("GTX", 3);
+        stock.put("Radeon", 8);
+        stock.put("Titan", 0);
+        stock.put("mouse", 40);
+        stock.put("keyboard", 30);
+        stock.put("hdd256", 0);
+        stock.put("hdd512", 0);
+        stock.put("hdd1024", 30);
+        stock.put("ssd256", 12);
+        stock.put("ssd512", 8);
+        stock.put("ssd1024", 0);
+        stock.put("RAM", 96);
 
         comboCPU.getItems().addAll(intel_cpus);
         comboRAM.getItems().addAll(rams);
@@ -75,8 +76,12 @@ public class CommercialController {
     }
 
     public void buy(ActionEvent actionEvent) throws Exception {
+        HashMap<String, Boolean> availability = new HashMap<>();
+        for(Map.Entry m : stock.entrySet()){
+            if(!m.getKey().equals("RAM") && !m.getKey().equals("hdd"))
+        }
         HashSet<Statement> knowledge = new HashSet<Statement>();
-        for (Map.Entry m : availability.entrySet()) {
+        for (Map.Entry m : stock.entrySet()) {
             Statement s = new Statement(
                     m.getKey() + "_A",
                     Type.BOOLEAN,
